84dd94f05ee184c5c3b7adb9c61e58714db3652d
Shinichiro <nuno1216shin@gmail.com>
2023-01-27 23:09:39 +0900

    easy

---
+++ test.js
@@ -0,0 +1,23 @@
+// const apple = "りんご";
+// console.log(apple);
+
+// let weight = 70;
+// weight = 60;
+// console.log(weight);
+
+// const answer = "あいうえお";
+// console.log(answer);
+
+// let answer = "あいうえお";
+// answer = "かきくけこ";
+// console.log(answer);
+
+// const str1 = "あいうえお";
+// const str2 = "かきくけこ";
+// const answer = str1 + str2;
+// console.log(answer);
+
+const str = "あいうえお";
+const newStr = str.replace("あ", "お");
+console.log(str);
+console.log(newStr);
