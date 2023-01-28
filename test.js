af571869ea6c5ec0421a683d0e89199315c60abf
Shinichiro <nuno1216shin@gmail.com>
2023-01-28 20:43:58 +0900

    76行目

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-const arr = ["りんご", "みかん", "いちご"];
-arr[2] = "もも";
-console.log(arr);
+const array = [1, 2, 3, 4, 5];
+
+const result = (arr) => {
+  let total = 0;
+  for(let i = 0; i < arr.length; i++)
+  total += arr[i];
+  return total;
+}
+
+console.log(result(array));
