05b0a965fc45b58b82cd32f497faa04598c5b73d
Shinichiro <nuno1216shin@gmail.com>
2023-01-28 21:20:30 +0900

    102行目

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-const object = [
-  {id: 1, name: "John", age: 15},
-  {id: 2, name: "Bob", age: 20},
-  {id: 3, name: "Michael", age: 15}
-];
-console.log(object[2].age);
+const person = {name: "Mike", age: 23};
+const personInfo = {...person, ...{country: "America", job: "software engineer"}};
+console.log(personInfo);
