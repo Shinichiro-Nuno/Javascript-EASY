ed96ad1850e82c0a0e997ae9f88076ef69ae71e8
Shinichiro <nuno1216shin@gmail.com>
2023-01-28 13:34:50 +0900

    37行目

--- test.js
+++ test.js
@@ -1,23 +1,6 @@
-// const apple = "りんご";
-// console.log(apple);
+const person = {
+  name: "ボブ",
+  age: 20
+}
 
-// let weight = 70;
-// weight = 60;
-// console.log(weight);
-
-// const answer = "あいうえお";
-// console.log(answer);
-
-// let answer = "あいうえお";
-// answer = "かきくけこ";
-// console.log(answer);
-
-// const str1 = "あいうえお";
-// const str2 = "かきくけこ";
-// const answer = str1 + str2;
-// console.log(answer);
-
-const str = "あいうえお";
-const newStr = str.replace("あ", "お");
-console.log(str);
-console.log(newStr);
+console.log(person);
