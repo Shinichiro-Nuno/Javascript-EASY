1e8165b3289213024933f52d1b7c34e2358afcfb
Shinichiro <nuno1216shin@gmail.com>
2023-01-29 12:00:14 +0900

    117行目

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
-// const item = {
-//   price: 100
+// const message = "アラートです。"
+
+// alert(message);
+
+// const name = "鈴木";
+
+// if(confirm(name + "さんで間違い無いですか？")) {
+//   console.log("ようこそ" + name + "さん");
+// } else {
+//   console.log("正しい名前を入力してください");
 // }
 
-// const {price} = item;
+const name = "田中";
 
-// console.log(price);
-
-const test = 10 > 15 ? true : false;
-
-console.log(test);
+if(confirm(`${name}さんで間違い無いですか？`)) {
+  console.log(`ようこそ${name}さん`)
+} else {
+  console.log(`正しい名前を入力してください`)
+}
